ae4c88a6ed0516e7282dcfe46152a631728bfa3f
COWells <charliewells270501@gmail.com>
2023-12-03 15:55:01 +0000

    Day 3 completed, This would be a lot neater if I had a Part record instead of a class.
    Might be worth upgrading to newer C# version to get these features

---
+++ Days/Day3/Part.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode2023
+{
+    public class Part
+    {
+        public string Text { get; private set; }
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+        public int Int => int.Parse(Text);
+        public Part(string text, int row, int column)
+        {
+            Text = text;
+            Row = row;
+            Column = column;
+        }
+    }
+}
