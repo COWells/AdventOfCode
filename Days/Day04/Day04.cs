1bc959871db4916e5705f46c8ef858b2f59f70df
COWells <charliewells270501@gmail.com>
2023-12-03 19:33:10 +0000

    Preparation for Day4

---
+++ Days/Day04/Day04.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2023
+{
+    public class Day04 : AdventDay
+    {
+        public override string CurrentDay => "Day04";
+
+        public override void SolveTask1(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SolveTask2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
