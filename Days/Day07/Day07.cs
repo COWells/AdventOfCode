2533c4c366f5a5e6b92b940942e3b706d71a2a9e
COWells <charliewells270501@gmail.com>
2023-12-06 12:19:00 +0000

    Preparing for Day07

---
+++ Days/Day07/Day07.cs
@@ -0,0 +1,24 @@
+﻿using CommonUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    public class Day07 : AdventDay
+    {
+        public override string CurrentDay => "Day07";
+
+        public override void SolveTask1(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SolveTask2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
